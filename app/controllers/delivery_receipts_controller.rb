672fab2caf55557a1deda193d66e24f96d01b9dc
Cristiano Betta <cristiano@betta.io>
2017-06-29 13:40:13 +0100

    Update delivery reciept controller to handle setup of webhook

--- app/controllers/delivery_receipts_controller.rb
+++ app/controllers/delivery_receipts_controller.rb
@@ -1,9 +1,15 @@
 class DeliveryReceiptsController < ApplicationController
   skip_before_action :verify_authenticity_token
+
+  # Updates an SMS message's status when a valid
+  # delivery receipt has been received
   def create
-    sms = Sms.find_by_message_id(params[:messageId])
-    sms.update_attributes(
-      status: params[:status]
-    )
+    # Find the SMS for the Message ID, and update it with the
+    # status, only if the Message ID was provided
+    Sms.where(message_id: params[:messageId])
+       .update_all(status: params[:status]) if params[:messageId]
+
+    # Return an empty HTTP 200 status
+    head :ok
   end
 end
