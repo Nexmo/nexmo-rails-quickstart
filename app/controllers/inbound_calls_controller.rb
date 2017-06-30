206296d6429743e88da672b66e31b6d4c8701d41
Cristiano Betta <cristiano@betta.io>
2017-06-30 18:14:34 +0100

    Fix comments

--- app/controllers/inbound_calls_controller.rb
+++ app/controllers/inbound_calls_controller.rb
@@ -5,14 +5,11 @@ class InboundCallsController < ApplicationController
   # Stores an inbound Call's details when a webhook
   # from Nexmo is received
   def create
-    # Creates a new Call record in
-    # the database
+    # Creates a new Call record in the database.
+    # Storing these details is not really necessary to make the call
+    # but it's useful for keeping track what calls have been made
     Call.where(conversation_uuid: params[:conversation_uuid])
-        # If a call event has already come in on call_events#create then
-        # don't create a new call, unstead use the one we already have
         .first_or_create
-        # Storing these details is not really necessary to make the call
-        # but it's useful for keeping track what calls have been made
         .update_attributes(
           # Update to store the Nexmo number this call was made to
           to: params[:to],
