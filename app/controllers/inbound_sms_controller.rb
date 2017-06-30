8b0a0a5f195853e41d1ca5b74d0da57f90eb3b9c
Cristiano Betta <cristiano@betta.io>
2017-06-30 14:36:46 +0100

    Update inbound SMS demo to reverse message

--- app/controllers/inbound_sms_controller.rb
+++ app/controllers/inbound_sms_controller.rb
@@ -7,7 +7,7 @@ class InboundSmsController < ApplicationController
   def create
     # Creates a new SMS record in
     # the database
-    Sms.create(
+    sms = Sms.create(
       to: params[:to],
       # The number that send the SMS is called a
       # MSISDN in this webhook
@@ -18,7 +18,30 @@ class InboundSmsController < ApplicationController
       is_inbound: true
     )
 
+    # Send a reply
+    reply sms
+
     # Return an empty HTTP 200 status regardless
     head :ok
+  end
+
+  private
+
+  # Initializes the Nexmo API client
+  def nexmo
+    # We do not pass in any API key or secret as
+    # we're using environment variables `NEXMO_API_KEY`
+    # and `NEXMO_API_SECRET`
+    client = Nexmo::Client.new
+  end
+
+  # Uses the Nexmo API to send a quick reply to the SMS received
+  # for simplicity we're not storing this one in the database
+  def reply sms
+    nexmo.send_message(
+      from: sms.to,
+      to: sms.from,
+      text: sms.text.reverse
+    )
   end
 end
